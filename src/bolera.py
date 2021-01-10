7f5e7ea3ca6b780adb9930a761fb5f15be0e4e8c
VictorPorlan <vporlan@cifpfbmoll.eu>
2021-01-10 12:52:48 +0100

    feat: :zap: Creado un divisor de turnos
    Ahora se puede dividir la partida en los turnos que se han hecho. No tengo claro si esto ayudará a que sea más facil calcularlo, pero en la teoria deberia serlo

--- src/bolera.py
+++ src/bolera.py
@@ -2,16 +2,37 @@ class Bolera:
     resultado = 0
     pleno = 10
     fallo = 0
-    max_turnos = 10
-
+    
     def __init__(self,puntos):
         self.puntos = list(puntos)
     
+    def divisor_turnos(self):
+        lista = []
+        partida = []
+        turnos = 0
+        for numero in self.puntos:
+            lista.append(numero)
+            if 'X' in lista and turnos == 9 and len(lista) == 3:
+                partida.append(lista)
+                lista = []
+            if '/' in lista and turnos == 9 and len(lista) == 3:
+                partida.append(lista)
+                lista = []
+            if 'X' not in lista and "/" not in lista and turnos == 9 and len(lista) == 2:
+                partida.append(lista)
+                lista = []
+            if len(lista) == 2 and turnos != 9:
+                partida.append(lista)
+                lista = []
+                turnos += 1
+            elif "X" in lista and turnos != 9:
+                partida.append(lista)
+                lista = []
+                turnos += 1
+        return partida
+
     def recorrer_lista(self):
         for numero in self.puntos:
             Bolera.resultado = Bolera.resultado + int(numero)
         return Bolera.resultado
-    
-
-
-
+print (Bolera("XXXXXXXXXXXX").divisor_turnos())
--- test/test_bolera.py
+++ test/test_bolera.py
@@ -2,3 +2,9 @@ from src.bolera import Bolera
 
 def test_basico_bolera():
     assert 60 == Bolera("12345123451234512345").recorrer_lista()
+def test_creador_turnos():
+    assert [['1','2'],['3','4'],['5','1'],['2','3'],['4','5'],['1','2'],['3','4'],['5','1'],['2','3'],['4','5']] == Bolera("12345123451234512345").divisor_turnos()
+    assert [['X'],['3','/'],['5','-'],['X'],['4','5'],['1','2'],['3','4'],['5','1'],['2','3'],['4','5']] == Bolera("X3/5-X451234512345").divisor_turnos()
+    assert [['X'],['3','/'],['5','-'],['X'],['4','5'],['1','2'],['3','4'],['5','1'],['2','3'],['4','5']] == Bolera("X3/5-X451234512345").divisor_turnos()
+    assert [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']] == Bolera("XXXXXXXXXXXX").divisor_turnos()
+    assert [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['4','/','X']] == Bolera("XXXXXXXXX4/X").divisor_turnos()
