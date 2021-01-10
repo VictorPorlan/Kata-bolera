ab225c3262235182d61dd42d7d67fc61cba5fed8
VictorPorlan <vporlan@cifpfbmoll.eu>
2021-01-10 17:28:57 +0100

    feat: :zap: Completado el kata. Pasa todos los casos tests.
    El uso de POO es nefasto, es necesaria refactorización para hacer mejor uso de esto. Pero al menos está completado

--- src/bolera.py
+++ src/bolera.py
@@ -1,38 +1,120 @@
 class Bolera:
-    resultado = 0
-    pleno = 10
-    fallo = 0
-    
+
     def __init__(self,puntos):
         self.puntos = list(puntos)
-    
-    def divisor_turnos(self):
-        lista = []
-        partida = []
-        turnos = 0
-        for numero in self.puntos:
-            lista.append(numero)
-            if 'X' in lista and turnos == 9 and len(lista) == 3:
-                partida.append(lista)
-                lista = []
-            if '/' in lista and turnos == 9 and len(lista) == 3:
-                partida.append(lista)
-                lista = []
-            if 'X' not in lista and "/" not in lista and turnos == 9 and len(lista) == 2:
-                partida.append(lista)
-                lista = []
-            if len(lista) == 2 and turnos != 9:
-                partida.append(lista)
-                lista = []
-                turnos += 1
-            elif "X" in lista and turnos != 9:
-                partida.append(lista)
-                lista = []
-                turnos += 1
-        return partida
 
     def recorrer_lista(self):
+        turnos = 0
+        resultado = 0
+        pleno = 10
+        acumulador_pleno = 0
+        acumulador_pleno_2 = 0
+        acumulador_spare = 0
+        ultimo_numero = 0
+        tiradas_en_el_turno = 0
         for numero in self.puntos:
-            Bolera.resultado = Bolera.resultado + int(numero)
-        return Bolera.resultado
-print (Bolera("XXXXXXXXXXXX").divisor_turnos())
+            valor = 0
+            if numero.isdigit() and turnos == 9:
+                valor += int(numero)
+                if acumulador_pleno > 0:
+                    valor += int(numero)
+                    acumulador_pleno -= 1
+                if acumulador_pleno_2 > 0:
+                    valor += int(numero)
+                    acumulador_pleno_2 -= 1
+                if acumulador_spare == 1:
+                    valor += int(numero)
+                    acumulador_spare -= 1
+                resultado += valor
+                ultimo_numero = int(numero)
+            if numero == "X" and turnos == 9:
+                valor += pleno
+                if acumulador_pleno > 0:
+                    valor += pleno
+                    acumulador_pleno -= 1
+                if acumulador_pleno_2 > 0:
+                    valor += pleno
+                    acumulador_pleno_2 -= 1
+                if acumulador_spare == 1:
+                    valor += pleno
+                    acumulador_spare -= 1
+                resultado += valor
+            if numero == "-" and turnos == 9:
+                if acumulador_pleno > 0:
+                    acumulador_pleno -= 1
+                if acumulador_pleno_2 > 0:
+                    acumulador_pleno_2 -= 1
+                if acumulador_spare >= 0:
+                    acumulador_spare = 0
+                ultimo_numero = 0
+            if numero == "/" and turnos == 9:
+                valor = 10 - ultimo_numero
+                if acumulador_pleno > 0:
+                    valor = 10 - ultimo_numero
+                    acumulador_pleno -= 1
+                resultado += valor
+            if numero == "X" and turnos != 9:
+                valor += pleno
+                if acumulador_pleno > 0:
+                    valor += pleno
+                    acumulador_pleno -= 1
+                if acumulador_pleno_2 > 0:
+                    valor += pleno
+                    acumulador_pleno_2 -= 1
+                if acumulador_spare == 1:
+                    valor += pleno
+                    acumulador_spare -= 1
+                if acumulador_pleno == 0:
+                    acumulador_pleno = 2
+                else:
+                    acumulador_pleno_2 = 2
+                resultado += valor
+                valor = 0
+                tiradas_en_el_turno = 0
+                turnos += 1
+            if numero == "/" and turnos != 9:
+                valor = 10 - ultimo_numero
+                acumulador_spare += 1
+                if acumulador_pleno > 0:
+                    valor += 10 - ultimo_numero
+                    acumulador_pleno -= 1
+                if acumulador_pleno_2 > 0:
+                    valor += 10 - ultimo_numero
+                    acumulador_pleno_2 -= 1
+                tiradas_en_el_turno = 0
+                turnos += 1
+                resultado += valor
+                valor = 0
+            if numero == "-" and turnos != 9:
+                if acumulador_pleno > 0:
+                    acumulador_pleno -= 1
+                if acumulador_pleno_2 > 0:
+                    acumulador_pleno_2 -= 1
+                if acumulador_spare >= 0:
+                    acumulador_spare = 0
+                tiradas_en_el_turno += 1
+                if tiradas_en_el_turno == 2:
+                    tiradas_en_el_turno = 0
+                    turnos += 1
+                ultimo_numero = 0
+                valor = 0
+            if numero.isdigit() and turnos != 9:
+                valor += int(numero)
+                if acumulador_pleno > 0:
+                    valor += int(numero)
+                    acumulador_pleno -= 1
+                if acumulador_pleno_2 > 0:
+                    valor += int(numero)
+                    acumulador_pleno_2 -= 1
+                if acumulador_spare == 1:
+                    valor += int(numero)
+                    acumulador_spare -= 1
+                tiradas_en_el_turno += 1
+                if tiradas_en_el_turno == 2:
+                    tiradas_en_el_turno = 0
+                    turnos += 1
+                resultado += valor
+                ultimo_numero = int(numero)
+                valor = 0
+        return resultado
+
--- test/test_bolera.py
+++ test/test_bolera.py
@@ -1,10 +1,11 @@
 from src.bolera import Bolera
 
-def test_basico_bolera():
+def test_bolera():
     assert 60 == Bolera("12345123451234512345").recorrer_lista()
-def test_creador_turnos():
-    assert [['1','2'],['3','4'],['5','1'],['2','3'],['4','5'],['1','2'],['3','4'],['5','1'],['2','3'],['4','5']] == Bolera("12345123451234512345").divisor_turnos()
-    assert [['X'],['3','/'],['5','-'],['X'],['4','5'],['1','2'],['3','4'],['5','1'],['2','3'],['4','5']] == Bolera("X3/5-X451234512345").divisor_turnos()
-    assert [['X'],['3','/'],['5','-'],['X'],['4','5'],['1','2'],['3','4'],['5','1'],['2','3'],['4','5']] == Bolera("X3/5-X451234512345").divisor_turnos()
-    assert [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']] == Bolera("XXXXXXXXXXXX").divisor_turnos()
-    assert [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['4','/','X']] == Bolera("XXXXXXXXX4/X").divisor_turnos()
+    assert 90 == Bolera("9-9-9-9-9-9-9-9-9-9-").recorrer_lista()
+    assert 150 == Bolera('5/5/5/5/5/5/5/5/5/5/5').recorrer_lista()
+    assert 149 == Bolera('8/549-XX5/53639/9/X').recorrer_lista()
+    assert 111 == Bolera('9-9-9-9-9-9-9-9-9-XXX').recorrer_lista()
+    assert 141 == Bolera('XXX9-9-9-9-9-9-9-').recorrer_lista()
+    assert 175 == Bolera('X5/X5/XX5/--5/X5/').recorrer_lista()
+    assert 120 == Bolera('XX9-9-9-9-9-9-9-9-').recorrer_lista()
